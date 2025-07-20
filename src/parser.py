2f3f633d002c33b3a7612b32b9b2beef5d15a98d
Vinuthna-Sneha <vinuthnasnehaathota@gmail.com>
2025-07-20 23:20:46 +0530

    updated parser

---
+++ src/parser.py
@@ -0,0 +1,106 @@
+class Node:
+    def __init__(self, type, value=None, children=None):
+        self.type = type
+        self.value = value
+        self.children = children or []
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.pos = 0
+
+    def consume(self, expected_type):
+        if self.pos < len(self.tokens) and self.tokens[self.pos].type == expected_type:
+            token = self.tokens[self.pos]
+            self.pos += 1
+            return token
+        raise SyntaxError(f"Expected {expected_type} at line {self.tokens[self.pos].line}")
+
+    def parse(self):
+        return Node("Program", children=self.parse_program())
+
+    def parse_program(self):
+        functions = []
+        while self.pos < len(self.tokens) and self.tokens[self.pos].type == 'FN':
+            functions.append(self.parse_function())
+        return functions
+
+    def parse_function(self):
+        self.consume('FN')
+        ident = self.consume('IDENT')
+        self.consume('LPAREN')
+        self.consume('RPAREN')
+        self.consume('LBRACE')
+        statements = []
+        while self.pos < len(self.tokens) and self.tokens[self.pos].type != 'RBRACE':
+            statements.append(self.parse_statement())
+        self.consume('RBRACE')
+        return Node("Function", ident.value, statements)
+
+    def parse_statement(self):
+        if self.tokens[self.pos].type == 'LET':
+            return self.parse_let_stmt()
+        elif self.tokens[self.pos].type == 'PARFOR':
+            return self.parse_parfor_stmt()
+        elif self.tokens[self.pos].type == 'PRINT':
+            return self.parse_print_stmt()
+        elif self.tokens[self.pos].type == 'RETURN':
+            return self.parse_return_stmt()
+        raise SyntaxError(f"Unexpected token at line {self.tokens[self.pos].line}")
+
+    def parse_let_stmt(self):
+        self.consume('LET')
+        ident = self.consume('IDENT')
+        self.consume('=')
+        expr = self.parse_expression()
+        self.consume('SEMICOLON')
+        return Node("Let", ident.value, [expr])
+
+    def parse_parfor_stmt(self):
+        self.consume('PARFOR')
+        ident = self.consume('IDENT')
+        self.consume('IN')
+        expr = self.parse_expression()
+        self.consume('LBRACE')
+        statements = []
+        while self.pos < len(self.tokens) and self.tokens[self.pos].type != 'RBRACE':
+            statements.append(self.parse_statement())
+        self.consume('RBRACE')
+        return Node("ParFor", ident.value, [expr] + statements)
+
+    def parse_print_stmt(self):
+        self.consume('PRINT')
+        self.consume('LPAREN')
+        expr = self.parse_expression()
+        self.consume('RPAREN')
+        self.consume('SEMICOLON')
+        return Node("Print", children=[expr])
+
+    def parse_return_stmt(self):
+        self.consume('RETURN')
+        expr = self.parse_expression()
+        self.consume('SEMICOLON')
+        return Node("Return", children=[expr])
+
+    def parse_expression(self):
+        if self.tokens[self.pos].type == 'NUMBER':
+            return Node("Number", self.consume('NUMBER').value)
+        elif self.tokens[self.pos].type == 'IDENT':
+            return Node("Identifier", self.consume('IDENT').value)
+        elif self.tokens[self.pos].type == 'LBRACKET':
+            return self.parse_array()
+        else:
+            left = self.parse_expression()
+            op = self.consume('OPERATOR')
+            right = self.parse_expression()
+            return Node("BinaryExpr", op.value, [left, right])
+
+    def parse_array(self):
+        self.consume('LBRACKET')
+        elements = []
+        while self.pos < len(self.tokens) and self.tokens[self.pos].type != 'RBRACKET':
+            elements.append(self.parse_expression())
+            if self.tokens[self.pos].type == 'COMMA':
+                self.consume('COMMA')
+        self.consume('RBRACKET')
+        return Node("Array", children=elements)
