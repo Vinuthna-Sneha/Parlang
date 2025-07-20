c7d2af885e2846ac87d19c89c87956eb9ff5273e
Vinuthna-Sneha <vinuthnasnehaathota@gmail.com>
2025-07-20 23:20:07 +0530

    updated lexer

---
+++ src/lexer.py
@@ -0,0 +1,43 @@
+import re
+
+class Token:
+    def __init__(self, type, value, line):
+        self.type = type
+        self.value = value
+        self.line = line
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source
+        self.pos = 0
+        self.line = 1
+        self.tokens = []
+        self.token_spec = [
+            ('NUMBER',   r'\d+'),           # Integer
+            ('IDENT',    r'[a-zA-Z]\w*'),  # Identifiers
+            ('LBRACE',   r'\{'),           # {
+            ('RBRACE',   r'\}'),           # }
+            ('LPAREN',   r'\('),           # (
+            ('RPAREN',   r'\)'),           # )
+            ('LBRACKET', r'\['),           # [
+            ('RBRACKET', r'\]'),           # ]
+            ('SEMICOLON', r';'),           # ;
+            ('COMMA',    r','),            # ,
+            ('OPERATOR', r'\+|-|\*|\/'),   # Operators
+            ('WHITESPACE', r'\s+'),        # Whitespace
+        ]
+        self.token_re = '|'.join(f'(?P<{pair[0]}>{pair[1]})' for pair in self.token_spec)
+        self.keywords = {'fn', 'let', 'parfor', 'in', 'print', 'return'}
+
+    def tokenize(self):
+        for match in re.finditer(self.token_re, self.source):
+            type = match.lastgroup
+            value = match.group()
+            if type == 'WHITESPACE':
+                if '\n' in value:
+                    self.line += value.count('\n')
+                continue
+            if type == 'IDENT' and value in self.keywords:
+                type = value.upper()
+            self.tokens.append(Token(type, value, self.line))
+        return self.tokens
